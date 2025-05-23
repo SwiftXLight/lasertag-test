caa3a7b73bad0b40e5ac8ce3a6946ec1f6384131
Andrii Panchenko <andrevpanchenko@gmail.com>
2025-05-23 15:07:05 +0300

    fix typo in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # Load .env vars and pass only REACT_APP_ vars to build args
 args=$(grep REACT_APP_ .env | sed 's/^/--build-arg /' | tr '\n' ' ')
 
-docker build $args -t my-react-app .
+docker build $args -t lasertag-test-app .
